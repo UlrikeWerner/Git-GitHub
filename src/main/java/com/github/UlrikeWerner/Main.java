e28fe782721f4827e408ac6811380a8dfc26e7a0
UlrikeWerner <ulrike_werner@web.de>
2023-09-08 11:37:46 +0200

    sayHello

---
+++ src/main/java/com/github/UlrikeWerner/Main.java
@@ -0,0 +1,11 @@
+package com.github.UlrikeWerner;
+
+public class Main {
+    public static void main(String[] args) {
+        sayHello("Hans");
+    }
+
+    public static void sayHello(String name){
+        System.out.println("Hello " + name + "!");
+    }
+}
